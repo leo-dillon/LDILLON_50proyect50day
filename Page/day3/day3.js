1b448d80b58d90080482248b82eb5f6b70d17fa7
Leonardo Dillon <jeannotegui@gmail.com>
2024-02-06 15:39:16 -0300

    Arreglando problemas de accesibilidad de los botones del menu

--- Page/day3/day3.js
+++ Page/day3/day3.js
@@ -1,17 +1,20 @@
 /* BÚSQUEDA */
 
 const btnCierreMenu = document.querySelector('.btnCierreMenu')
-const btnMenu = document.querySelector('.btnMenu')
+const btnMenu = document.querySelector('.abrir')
 const contenedorDog = document.querySelector('.contenedorGoblal')
 console.log(contenedorDog.style.rotate)
 
 
-btnMenu.addEventListener('click', (button)=>{
-   button.target.parentElement.style.rotate = -180 + 'deg'
+btnMenu.addEventListener('click', e=>{
+   e.target.parentElement.parentElement.style.rotate = -180 + 'deg'
+
+
+
    contenedorDog.style.rotate = +10 + 'deg'
 
 })
-btnCierreMenu.addEventListener('click', (button)=>{
-   button.target.parentElement.style.rotate = 0 + 'deg'
+btnCierreMenu.addEventListener('click', (e)=>{
+   e.target.parentElement.parentElement.style.rotate = 0 + 'deg'
    contenedorDog.style.rotate = +0 + 'deg'
 })
